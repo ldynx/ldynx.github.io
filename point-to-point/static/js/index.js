43038649e83a1ae92cbb465fd1230f1dbb0ab9a6
ldynx <ldynx@snu.ac.kr>
2025-11-20 20:17:10 -0800

    Update index.js

--- point-to-point/static/js/index.js
+++ point-to-point/static/js/index.js
@@ -30,8 +30,8 @@ $(document).ready(function() {
     });
 
     var options = {
-			slidesToScroll: 3,
-			slidesToShow: 3,
+			slidesToScroll: 4,
+			slidesToShow: 4,
 			loop: true,
 			infinite: true,
 			autoplay: false,
